875e7c2917ea4353c043d691030a07e56f08eded
greyy-nguyen <nhan.nguyen@skymavis.com>
2025-01-02 14:12:45 +0700

    chore: add ronin wallet build file

---
+++ examples/metamask/.cache-synpress/ronin-wallet-chrome/assets/js/index.DkvvEwj1.js
@@ -0,0 +1,1 @@
+var C=(c=>(c.ACCOUNTS_CHANGED="accounts_changed",c.ACCOUNT_CHANGED="account_changed",c.CHAIN_ID_CHANGED="chain_id_changed",c.UNLOCK_CHANGED="unlock_changed",c))(C||{}),_=(c=>(c.POPUP_CLOSED="popup-closed",c.START_TIMEOUT_LOCK="newTimeoutLock",c.STOP_TIMEOUT_LOCK="stopTimeoutLock",c.LOCK_EXTENSION="lockExtension",c))(_||{});export{_ as G,C as R};
