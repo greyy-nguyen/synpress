32a5f81fe641dd67ca900e4be5884423b1fce669
greyy-nguyen <nhan.nguyen@skymavis.com>
2025-01-03 14:35:18 +0700

    feat: change dapp test to tanto kit example

--- examples/metamask/test/wallet-setup/connected.setup.ts
+++ examples/metamask/test/wallet-setup/connected.setup.ts
@@ -5,6 +5,8 @@ import "dotenv/config";
 const SEED_PHRASE = process.env.SEED_PHRASE;
 const PASSWORD = process.env.WALLET_PASSWORD;
 
+const PAGE_STABLE_LOADED_TIMEOUT = 1000;
+
 export default defineWalletSetup(PASSWORD, async (context, walletPage) => {
   const roninWallet = new RoninWallet(context, walletPage, PASSWORD);
 
@@ -13,12 +15,11 @@ export default defineWalletSetup(PASSWORD, async (context, walletPage) => {
   const page = await context.newPage();
 
   // Go to a locally hosted MetaMask Test Dapp.
-  await page.goto("http://localhost:9999");
+  await page.goto("https://mavis-sdk-examples.vercel.app/tanto-kit");
+  await page.waitForTimeout(PAGE_STABLE_LOADED_TIMEOUT);
 
   // Choose the first provider from injected providers.
-  await page.locator(".eip6963-providers #provider:first-child button").click();
-
-  await page.locator("#connectButton").click();
+  await page.locator('div[class*="ConnectButton"] button').click();
 
   await roninWallet.connectToDapp(["Account 1"]);
 });
